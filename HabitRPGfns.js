9a307cb9f5009caba69a97c6ec241a6c4d30f1ba
sanzoghenzo <andrea.ghensi@gmail.com>
2016-02-02 12:58:35 +0100

    Improved Score task and tag functions added
    - updated BASE_URL with new domain
    - Improved "score_task" function:
        - bugfix: function always scoring in up direction
        - down scoring only if task is a bad habit (type='habit', down=true)
    - added "get_tasks_by_tag_name" function and helper functions to get a comma separated list of today's tasks with that tag
    - created "get_today" helper function from part of "get_due" function in order to use it with "get_tasks_by_tag_name" function

--- HabitRPGfns.js
+++ HabitRPGfns.js
@@ -4,8 +4,8 @@
 // setGlobal('HabitrpgCDS', '0');  // Custom Day Start value, 0 if not set
 
 // A constant and two variables used by most of the functions
-var BASE_URL = 'https://habitrpg.com:443/api/v2';
-var result = "";
+var BASE_URL = 'https://habitica.com:443/api/v2';
+var result = '';
 var results = [];
 
 
@@ -47,29 +47,56 @@ function add_task() {
 		}
 	}
 
-	result = callAPI("POST", '/user/tasks', '{ ' + jsonstr + ' }');
+	result = callAPI('POST', '/user/tasks', '{ ' + jsonstr + ' }');
 }
-
 
 // Input: taskid, direction (default "up")
 // End result: delta (arbitrarily chosen)
 // End results: lvl, hp, exp, mp, gp (after-value, not difference)
 //http://blog.andrew.net.au/2014/08/05
 function score_task() {
-	var direction = direction || "up";
-	var p = JSON.parse(callAPI("POST", '/user/tasks/' + taskid + '/' + direction));
-	result = p.delta;  // We could alternatively return one of gp/exp/mp/hp
-	results = [ p.lvl, p.hp, p.exp, p.mp, p.gp ];
+	var dir = direction || 'up';
+	var runnable = true;
+	if (dir == 'down') {
+		//only for testing
+		// don't downscore if it's not a bad habit, check type and down
+		var q = JSON.parse(callAPI("GET", '/user/tasks/' + taskid));
+		if ((q.type == 'habit' && q.down == false) || q.type == 'daily' || q.type == 'todo') {
+			//flash ('Ignoring down score');
+			runnable = false;
+		} 
+	} 
+	if (runnable) {
+		var p = JSON.parse(callAPI("POST", '/user/tasks/' + taskid + '/' + dir));
+		result = p.delta;  // We could alternatively return one of gp/exp/mp/hp
+		results = [ p.lvl, p.hp, p.exp, p.mp, p.gp ];
+	}
 }
 
 // Input: taskid
 // End results: completed, text, notes, value, priority, streak
 // guessed from http://blog.andrew.net.au/2014/08/05
 function query_task() {
-	var p = JSON.parse(callAPI("GET", '/user/tasks/' + taskid));
+	var p = JSON.parse(callAPI('GET', '/user/tasks/' + taskid));
 	results = [ p.completed, p.text, p.notes, p.value, p.priority, p.streak ];
 }
 
+// Input: tagname
+// End result: string (with linefeeds) of id,text,notes
+// End results: array of id,text,notes
+// Used to build today checklist (and notifications)
+function get_tasks_by_tag_name() {
+	var today = get_today();
+	var p = get_tasks_by_tag_id(get_tag_id_by_name(tagname));
+	for (var key in p) {
+		if (p[key].type == 'daily' && !(p[key].repeat[today])) {
+			continue;
+		}
+		var txt = p[key].id + ',' + p[key].text + ',' + p[key].notes;
+		result += txt + '\n';
+		results.push(txt);
+	}
+}
 
 // Input: (none)
 // End result: string (with linefeeds) of streak@title
@@ -78,27 +105,19 @@ function get_streaks() {
 	var p = get_all_tasks();
 	for (var key in p) {
 		if (p[key].streak) {
-			var txt = p[key].streak + "@" + p[key].text;
-			result += txt + "\n";
+			var txt = p[key].streak + '@' + p[key].text;
+			result += txt + '\n';
 			results.push(txt);
 		}
 	}
 }
-
 
 // Input: (none)
 // End result: string (with linefeeds) of non-gray dailys undone
 // End results: array of non-gray dailys undone
 // Hint: use %results(#) for count
 function get_due() {
-	var today = new Date();
-	// It would be cumbersome to get the entire user object just for this
-	// so expect that if one uses custom day start, it's stored in Tasker.
-	var cds = global('HabitrpgCDS') || 0;
-
-	// credit: Alys's habitrpg_user_data_display.html collateDailiesData()
-	today.setHours(today.getHours() - cds);
-	today = ["su","m","t","w","th","f","s"][today.getDay()];
+	var today = get_today();
 
 	var p = get_all_tasks();
 	for (var key in p) {
@@ -107,39 +126,66 @@ function get_due() {
 			continue;
 		}
 		if (obj.repeat[today]) {
-			result += obj.text + "\n";
+			result += obj.text + '\n';
 			results.push(obj.text);
 		}
 	}
 }
 
+// This is a helper function for get_due and get_tasks_by_tag_name
+function get_today() {
+	var today = new Date();
+	// It would be cumbersome to get the entire user object just for this
+	// so expect that if one uses custom day start, it's stored in Tasker.
+	var cds = global('HabitrpgCDS') || 0;
+	// credit: Alys's habitrpg_user_data_display.html collateDailiesData()
+	today.setHours(today.getHours() - cds);
+	today = ['su','m','t','w','th','f','s'][today.getDay()];
+	return today;
+}
+
 
 // This is a helper function for get_streaks and get_due
 function get_all_tasks() {
-	return JSON.parse(callAPI("GET", '/user/tasks'));
+	return JSON.parse(callAPI('GET', '/user/tasks'));
 }
 
+function get_tasks_by_tag_id(tagId) {
+	return get_all_tasks().filter(function (el) {
+		return el.tags[tagId] == true;
+	});
+}
+
+function get_tag_id_by_name(tagName) {
+	return get_all_tags().filter(function (el) {
+		return el.name == tagName;
+	})[0].id;
+}
+
+function get_all_tags() {
+	return JSON.parse(callAPI('GET', '/user/tags'));
+}
 
 // This is a helper function for all calls
 function callAPI(method, partialURL, postData) {
 	var http = new XMLHttpRequest();
 	http.open(method, BASE_URL + partialURL, false);
-	http.setRequestHeader("Content-Type", "application/json");
-	http.setRequestHeader("x-api-user", global('HabitrpgUserid'));
-	http.setRequestHeader("x-api-key", global('HabitrpgApiToken'));
+	http.setRequestHeader('Content-Type', 'application/json');
+	http.setRequestHeader('x-api-user', global('HabitrpgUserid'));
+	http.setRequestHeader('x-api-key', global('HabitrpgApiToken'));
 	if (typeof postData !== 'undefined')  http.send(postData);
 	else                                  http.send();
 	return (http.responseText);
 }
 
-
 try {
 	switch (operation) {
-		case "add_task": add_task(); break;
-		case "score_task": score_task(); break;
-		case "query_task": query_task(); break;
-		case "get_streaks" : get_streaks(); break;
-		case "get_due" : get_due(); break;
+		case 'add_task': add_task(); break;
+		case 'score_task': score_task(); break;
+		case 'query_task': query_task(); break;
+		case 'get_streaks' : get_streaks(); break;
+		case 'get_due' : get_due(); break;
+		case 'get_tasks_by_tag': get_tasks_by_tag_name(); break;
 		default: flash('HabitRPGfns.js lacks "' + operation + '"');
 	}
 } catch (e) {
